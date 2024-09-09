6c083e0bc86f36cfbf28161bfb8c324af7702332
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2024-09-09 20:36:53 +0200

    Going live

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,9 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_st = configuration.database.connection_string();
-    let connection_pool = PgPool::connect(&connection_st.expose_secret())
-        .await
+    let connection_pool = PgPool::connect_lazy(&connection_st.expose_secret())
         .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = configuration.application.address();
     let listener = TcpListener::bind(address)
         .expect("Failed to bind random port");
     run(listener, connection_pool)?.await
